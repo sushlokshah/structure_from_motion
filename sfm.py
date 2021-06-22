8d47e4348ead15589f7b656bf57e6d0331c5658a
sushlokshah <sushlokshahkop3@gmail.com>
2021-06-22 22:40:19 +0530

    completed basic code and computed results.

--- sfm.py
+++ sfm.py
@@ -1,24 +1,13 @@
 #!/usr/bin/env python
 import os
 import matplotlib
-matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 from os import listdir
-import random
 import numpy as np
 import cv2 as cv
-import pandas as pd
-import open3d as o3d
 import time
 import struct
-
-dataset_location = 'C:\\Users\\sushl\\Desktop\\papers\\datasets\\GustavIIAdolf'
-L = os.listdir(dataset_location)
-L.sort()
-
-k = np.array([[2393.952166119461, -3.410605131648481e-13, 932.3821770809047], 
-            [0, 2398.118540286656, 628.2649953288065], 
-            [0, 0, 1]])
+import yaml
 
 def feature_detection(img,detector):
     clahe = cv.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
@@ -45,7 +34,7 @@ def feature_detection(img,detector):
     img2 = cv.drawKeypoints(img, kp, None, color=(0,255,0), flags=0)
     plt.imshow(img2)
     #plt.show()
-    plt.pause(0.001)
+    plt.pause(0.01)
     return kp,des
 
 def Triangulation(P1, P2, pts1, pts2):
@@ -127,9 +116,9 @@ def write_pointcloud(filename,xyz_points,rgb_points=None):
     print(temp.mean(axis = 0))
     dist = np.sqrt(temp[:, 0] ** 2 + temp[:, 1] ** 2 + temp[:, 2] ** 2)
     #print(dist.shape, np.mean(dist))
-    indx = np.where(dist < np.mean(dist)+ 1)
+    indx = np.where(dist < np.mean(dist)+ 2)
     xyz_points = xyz_points[indx]
-    rgb_points = np.ones(xyz_points.shape).astype(np.uint8)*255
+    rgb_points = rgb_points[indx]
     # Write header of .ply file
     fid = open(filename,'wb')
     fid.write(bytes('ply\n', 'utf-8'))
@@ -146,8 +135,8 @@ def write_pointcloud(filename,xyz_points,rgb_points=None):
     # Write 3D points to .ply file
     for i in range(xyz_points.shape[0]):
         fid.write(bytearray(struct.pack("fffccc",xyz_points[i,0],xyz_points[i,1],xyz_points[i,2],
-                                        rgb_points[i,0].tobytes(),rgb_points[i,1].tobytes(),
-                                        rgb_points[i,2].tobytes())))
+                                        rgb_points[i,2].tobytes(),rgb_points[i,1].tobytes(),
+                                        rgb_points[i,0].tobytes())))
     fid.close()
 
 def VO(L):
@@ -157,6 +146,9 @@ def VO(L):
     point_cloud = []
     trajectory = []
     is_point_cloud_avaliable = False
+    color = cv.imread(dataset_location +'/'+ L[0])
+    plt.imshow(color)
+    plt.show()
     img1 = cv.imread(dataset_location +'/'+ L[0],cv.IMREAD_GRAYSCALE)
     kp1,des1 = feature_detection(img1,"Sift")
     img2 = cv.imread(dataset_location +'/'+ L[1],cv.IMREAD_GRAYSCALE)
@@ -183,11 +175,17 @@ def VO(L):
     print(0,reprojection_error(point_cloud,P1,common_pts1))
     print(1,reprojection_error(point_cloud,P2,common_pts2))
     p3d = point_cloud
+    idx = common_pts1.reshape(-1,2).astype(np.uint8())
+    color_data = color[idx[:,0],idx[:,1]]
+    print(color)
+    print(p3d[:,:3].shape)
+    #write_pointcloud("abc.ply",p3d[:,:3],color[idx[:,0],idx[:,1]])
     #print(point_cloud.shape)
     for i in range(1,2):
         print(i)
         is_point_cloud_avaliable = True
         print(L[i],L[i+1])
+        color = cv.imread(dataset_location +'/'+ L[i])
         img3 = cv.imread(dataset_location +'/'+ L[i+1],cv.IMREAD_GRAYSCALE)
         kp3,des3 = feature_detection(img2,"Sift")
         #print(len(ckp2))
@@ -203,8 +201,11 @@ def VO(L):
         pts2,pts3,common_pts2,common_pts3,common_des2,common_des3,ckp2,ckp3 = keypoint_matches(kp2,des2,kp3,des3,point_cloud,is_point_cloud_avaliable)
         point_cloud = Triangulation(P2, P3, common_pts2, common_pts3)
         print(i,reprojection_error(point_cloud,P3,common_pts3))
+        idx = common_pts2.reshape(-1,2).astype(np.uint8())
         if(reprojection_error(point_cloud,P3,common_pts3) < 5):
             p3d = np.vstack((p3d,point_cloud))
+            print(color[idx[:,0],idx[:,1]])
+            color_data = np.vstack((color_data,color[idx[:,0],idx[:,1]]))
         img2 = img3
         kp2,des2 = kp3,des3
         pts2,common_pts2,common_des2,ckp2 = pts3,common_pts3,common_des3,ckp3
@@ -214,5 +215,100 @@ def VO(L):
     end = time.time()
     print ("Time elapsed:", end - start)
     
-    write_pointcloud("pt.ply",p3d[:,:3])
-VO(L)
+    write_pointcloud("pt.ply",p3d[:,:3],color_data)
+
+def find_trajectory(path,L):
+    start = time.time()
+    point_cloud = []
+    color_data = []
+    trajectory = []
+    is_point_cloud_avaliable = False
+
+    frame = 0
+    img_color = cv.imread(path +'/'+ L[frame])
+    print(frame)
+    img0 = cv.cvtColor(img_color, cv.COLOR_BGR2GRAY)
+    frame = frame + 1
+    kp0,des0 = feature_detection(img0,"Sift")
+    print(frame)
+    img1 = cv.imread(path +'/'+ L[frame],0)
+    frame = frame + 1
+    kp1,des1 = feature_detection(img1,"Sift")
+
+    pts0,pts1,common_pts0,common_pts1,common_des0,common_des1,ckp0,ckp1 = keypoint_matches(kp0,des0,kp1,des1,point_cloud,is_point_cloud_avaliable) 
+    
+    E,mask = cv.findEssentialMat(common_pts0,common_pts1,k,cv.RANSAC,0.999,1.0,1000)
+    common_pts0 = common_pts0[mask.ravel() == 1]
+    common_pts1 = common_pts1[mask.ravel() == 1]
+    common_des0 = common_des0[mask.ravel() == 1]
+    common_des1 = common_des1[mask.ravel() == 1]
+    
+    retval, R, t, mask = cv.recoverPose(E, common_pts0, common_pts1, k)
+    common_pts0 = common_pts0[mask.ravel() == 255]
+    common_pts1 = common_pts1[mask.ravel() == 255]
+    common_des0 = common_des0[mask.ravel() == 255]
+    common_des1 = common_des1[mask.ravel() == 255]
+    ckp0 = cv.KeyPoint_convert(common_pts0)
+    ckp1 = cv.KeyPoint_convert(common_pts1)
+    
+    print(len(common_pts0),len(common_pts1))
+    P1 = projection_matrix(k,np.eye(3).astype(np.float32),np.zeros((3,1))) 
+    P2 = projection_matrix(k,R,t)
+    point_cloud = Triangulation(P1, P2, common_pts0, common_pts1)
+    
+    print(0,reprojection_error(point_cloud,P1,common_pts0))
+    print(1,reprojection_error(point_cloud,P2,common_pts1))
+    
+    p3d = point_cloud
+    idx = common_pts0.reshape(-1,2).astype(np.uint8())
+    color_data = img_color[idx[:,0],idx[:,1]]
+    #print(color_data)
+    while(frame<len(L)):
+        print(frame)
+        is_point_cloud_avaliable = True
+        img1_color = cv.imread(path +'/'+ L[frame - 1])
+        img2 = cv.imread(path +'/'+ L[frame],cv.IMREAD_GRAYSCALE)
+        kp2,des2 = feature_detection(img2,"Sift")
+
+        pts1,pts2,common_pts1,common_pts2,common_des1,common_des2,ckp1,ckp2,point_cloud = keypoint_matches(ckp1,common_des1,kp2,des2,point_cloud,is_point_cloud_avaliable)
+
+        R1,t1 = pnp(point_cloud[:,:3],common_pts2,k)
+        is_point_cloud_avaliable = False
+
+        trajectory.append(t1)
+
+        P3 = projection_matrix(k,R1,t1)
+
+        pts1,pts2,common_pts1,common_pts2,common_des1,common_des2,ckp1,ckp2 = keypoint_matches(kp1,des1,kp2,des2,point_cloud,is_point_cloud_avaliable)
+        point_cloud = Triangulation(P2, P3, common_pts1, common_pts2)
+
+        print(frame,reprojection_error(point_cloud,P3,common_pts2))
+
+        idx = common_pts1.reshape(-1,2).astype(np.uint8())
+
+        p3d = np.vstack((p3d,point_cloud))
+
+        color_data = np.vstack((color_data,img1_color[idx[:,0],idx[:,1]]))
+        img1 = img2
+        kp1,des1 = kp2,des2
+        pts1,common_pts1,common_des1,ckp1 = pts2,common_pts2,common_des2,ckp2
+        #print(len(ckp2))
+        #print(len(point_cloud))
+        P2 = P3
+        frame = frame + 1
+    write_pointcloud("output.ply",p3d[:,:3],color_data.reshape(-1,3))
+    traj = np.asarray(trajectory).reshape((-1,3))
+    plt.figure()
+    plt.subplot(1, 1, 1, projection='3d').plot(traj[:, 0], traj[:, 1], traj[:, 2])
+    plt.show()
+
+
+if __name__ == "__main__":
+  with open('info.yaml') as f:
+    data = yaml.load(f, Loader=yaml.FullLoader)
+  path = data["dataset_location"]
+  k = np.array(data["k"])
+  L = os.listdir(path)
+  L.sort()
+  find_trajectory(path,L)
+  
